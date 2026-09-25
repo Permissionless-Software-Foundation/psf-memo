a9b196501399c42c4cf24a596716b222f77153da
Chris Troutner <chris.troutner@gmail.com>
2026-09-25 12:25:28 -0700

    Review TX indexer handoff failure logging
    Record the tool-written mutate4javascript manifests (adapter, handoff use
    case, use-case wiring) and the updated gherkin-mutation manifest covering the
    new failure-logging scenario. No survivors and no production changes.
    
    By architect.

--- psf-memo-indexer/src/adapters/tx-indexer.js
+++ psf-memo-indexer/src/adapters/tx-indexer.js
@@ -35,5 +35,5 @@ class TxIndexerAdapter {
 export default TxIndexerAdapter
 
 // mutate4javascript-manifest-begin
-// {"version":1,"tested_at":"2026-09-25T18:27:07.039Z","module_hash":"b2e38fff5c399ab525b3f86d5ad0984b4dab8fd4e906da2075a98ef8b772eea8","functions":[{"id":"func/TxIndexerAdapter.constructor","name":"TxIndexerAdapter.constructor","line":13,"end_line":16,"hash":"eca882ae1698511c8aef056c2d95b30b981af53b0be527a3a8c76219e1d7ae07"},{"id":"func/TxIndexerAdapter.startTxIndexer","name":"TxIndexerAdapter.startTxIndexer","line":18,"end_line":25,"hash":"7326457687b76aad089f069b73959a5fc5baa1934578ec45e1fa1bcbbb4e7d7d"}]}
+// {"version":1,"tested_at":"2026-09-25T19:23:00.532Z","module_hash":"4fdf7c3e86c97ec87efe85157b1a734e453b10721d6d53ba61e5935dc3dd9758","functions":[{"id":"func/TxIndexerAdapter.constructor","name":"TxIndexerAdapter.constructor","line":13,"end_line":16,"hash":"eca882ae1698511c8aef056c2d95b30b981af53b0be527a3a8c76219e1d7ae07"},{"id":"func/TxIndexerAdapter.startTxIndexer","name":"TxIndexerAdapter.startTxIndexer","line":18,"end_line":26,"hash":"491e3f5b934abe36ef690debfa18c9504c7baffd02bfd90a6e048abf11409964"},{"id":"func/TxIndexerAdapter.endpoint","name":"TxIndexerAdapter.endpoint","line":30,"end_line":32,"hash":"9b91248e877b2c8f1d81c8f2877b972f397329f6eb5013e1ab573415104fd928"}]}
 // mutate4javascript-manifest-end
--- psf-memo-indexer/src/use-cases/use-cases-index.js
+++ psf-memo-indexer/src/use-cases/use-cases-index.js
@@ -35,5 +35,5 @@ class UseCases {
 export default UseCases
 
 // mutate4javascript-manifest-begin
-// {"version":1,"tested_at":"2026-09-25T18:28:06.656Z","module_hash":"7f4265a51269317e76d4e7070f866c19f1a6c22bd641f56d82b12684b77a0be8","functions":[{"id":"func/UseCases.constructor","name":"UseCases.constructor","line":9,"end_line":25,"hash":"e421751064ccb1d41f73c273ac74d0ac31724b5f985face4dc11b70dc6acbca3"},{"id":"func/UseCases.initUseCases","name":"UseCases.initUseCases","line":27,"end_line":30,"hash":"2ecb643dc5a45c136593e0a19832058189503cd4f146a6a7cbe71608c84a4ea3"}]}
+// {"version":1,"tested_at":"2026-09-25T19:23:43.399Z","module_hash":"38a15a81f4817be165fc34b771723d42c94f5f29d2b9d978326d8700c2ee322d","functions":[{"id":"func/UseCases.constructor","name":"UseCases.constructor","line":9,"end_line":27,"hash":"ee5e293df0599dfc34bed1c2b7a9f983be4b31000ccf2d9899f5ba46b1cdac8f"},{"id":"func/UseCases.initUseCases","name":"UseCases.initUseCases","line":29,"end_line":32,"hash":"2ecb643dc5a45c136593e0a19832058189503cd4f146a6a7cbe71608c84a4ea3"}]}
 // mutate4javascript-manifest-end
