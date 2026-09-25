5076f073e965a1f1cf229280f0c6770b1a319e14
Chris Troutner <chris.troutner@gmail.com>
2026-09-25 14:11:55 -0700

    Record profile post rendering mutation manifests
    The new ProfilePostContent wrapper is a pure createElement delegation with
    no mutable language sites (mutation scan reports 0). The soft Gherkin run
    kills 33/35 mutations; the two survivors are case changes of surrounding
    text that scenario 4 never asserts. No production behavior changed.
    
    By architect.

--- psf-memo-client/src/components/app-body/profile/profile-post-content.js
+++ psf-memo-client/src/components/app-body/profile/profile-post-content.js
@@ -20,3 +20,7 @@ function ProfilePostContent ({ text = '', initialFailedImages }) {
 }
 
 module.exports = ProfilePostContent
+
+// mutate4javascript-manifest-begin
+// {"version":1,"tested_at":"2026-09-25T21:09:58.761Z","module_hash":"82f58305640f92a81a8ec065aa6af383d24ecd0515a8a7790f89f55e10a4cdd8","functions":[{"id":"func/ProfilePostContent","name":"ProfilePostContent","line":14,"end_line":20,"hash":"94b6a9cad1228c782c1b5cc8cccd305bb4a593c6a74bdd7cb218612008e9b90b"}]}
+// mutate4javascript-manifest-end
